7f35288839256e2e8824b4a38ddcd85dc46afa10
Florian <floriangeorges1992@gmail.com>
2024-12-05 11:52:45 +0100

    creation d'un arbre -DOM

--- src/index.js
+++ src/index.js
@@ -1,9 +1,63 @@
 import "./style.css";
-const p = document.querySelector("p");
-const img = document.querySelector("img");
-const section = document.querySelector("section");
-const a = document.querySelector("a");
-const input = document.querySelector("input");
+
+const app = document.querySelector("#app");
+
+const section = document.createElement("section");
+const image = document.createElement("img");
+const imageSrc = document.createAttribute("src");
+imageSrc.value =
+  "https://assistanteplus.fr/wp-content/uploads/2022/04/chat-midjourney.webp";
+image.setAttributeNode(imageSrc);
+
+const paragraphe = document.createElement("p");
+const paragrapheClass = document.createAttribute("class");
+paragrapheClass.value = "text-primary";
+const paragrapheText = document.createTextNode("je suis un paragraphe");
+
+paragraphe.setAttributeNode(paragrapheClass);
+
+const a = document.createElement("a");
+
+const aHref = document.createAttribute("href");
+aHref.value = "https://fr.wikipedia.org/wiki/Hello_world";
+a.setAttributeNode(aHref);
+
+const aText = document.createTextNode("Dyma");
+
+const comment = document.createComment("je suis un commentaire");
+
+//creation de l'arbre
+app.appendChild(section);
+section.append(image);
+section.insertAdjacentElement("beforeBegin", paragraphe);
+paragraphe.append(paragrapheText, a);
+a.appendChild(aText);
+app.appendChild(comment);
+// paragraphe.insertBefore(comment, a);
+
+// <section class="test">
+//   <img
+//     width="50px"
+//     height="50px"
+//     src="https://assistanteplus.fr/wp-content/uploads/2022/04/chat-midjourney.webp"
+//   />
+//   <p class="class-test">
+//     je suis un paragraphe
+//     <a href="https://fr.wikipedia.org/wiki/Hello_world">HELLO WORLD</a>
+//   </p>
+//   <input
+//     data-test="Jean"
+//     type="text"
+//     id="input"
+//     placeholder="Entrez votre message"
+//   />
+// </section>
+// `;}
+// const p = document.querySelector("p");
+// const img = document.querySelector("img");
+// const section = document.querySelector("section");
+// const a = document.querySelector("a");
+// const input = document.querySelector("input");
 
 //modification du contenu
 // p.childNodes[0].nodeValue = "SALUT";
@@ -28,15 +82,15 @@ const input = document.querySelector("input");
 // console.log(img.getAttribute("test"));
 // img.removeAttribute("test");
 
-console.log(input.dataset.test);
+// console.log(input.dataset.test);
 
-input.focus();
-setTimeout(() => {
-  input.blur();
-}, 1000);
+// input.focus();
+// setTimeout(() => {
+//   input.blur();
+// }, 1000);
 
 // input.type = "date";
-input.placeholder = "Hello";
+// input.placeholder = "Hello";
 
 //désactiver l'input
 // input.disabled = true;
@@ -46,9 +100,9 @@ input.placeholder = "Hello";
 // console.log(input.validity);
 // Mismatch: false, tooLong: false, tooShort: false, …}badInput: falsecustomError: falsepatternMismatch: falserangeOverflow: falserangeUnderflow: falsestepMismatch: falsetooLong: falsetooShort: falsetypeMismatch: falsevalid: truevalueMissing: false[[Prototype]]: ValidityState
 
-section.style.width = 100;
-section.style.height = 100;
-section.style.backgroundColor = "";
+// section.style.width = 100;
+// section.style.height = 100;
+// section.style.backgroundColor = "";
 
 // console.log(section.style);
 
@@ -59,14 +113,14 @@ section.style.backgroundColor = "";
 // console.log(section.className);
 // console.log(section.getAttribute("class"));
 // section.className = " red text-primary";
-section.classList.add("red");
-section.classList.remove("test");
-setTimeout(() => {
-  section.classList.add("red");
-  console.log(section.classList.contains("red"));
-}, 5000);
-setTimeout(() => {
-  section.classList.toggle("red");
-}, 10000);
+// section.classList.add("red");
+// section.classList.remove("test");
+// setTimeout(() => {
+//   section.classList.add("red");
+//   console.log(section.classList.contains("red"));
+// }, 5000);
+// setTimeout(() => {
+//   section.classList.toggle("red");
+// }, 10000);
 
-section.classList.toggle("red");
+// section.classList.toggle("red");
