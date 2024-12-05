1b6e5cc56e6f10d5f17b91cd6a56a9ce433778e1
Florian <floriangeorges1992@gmail.com>
2024-12-05 10:19:18 +0100

    update input validity

--- src/index.js
+++ src/index.js
@@ -1,2 +1,46 @@
-const element = document.querySelectorAll("img");
-console.log(element);
+const p = document.querySelector("p");
+const img = document.querySelector("img");
+const section = document.querySelector("section");
+const a = document.querySelector("a");
+const input = document.querySelector("input");
+
+//modification du contenu
+// p.childNodes[0].nodeValue = "SALUT";
+
+// p.innerHTML = "<strong>SALUT</strong>";
+
+// p.innerText = "yooo";
+
+//modification source de l'image
+// img.src =
+//   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwv178TD2B0QpRzs8RzWoGKiSZzS4HqvW12A&s";
+// img.attributes[1].nodeValue =
+//   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwv178TD2B0QpRzs8RzWoGKiSZzS4HqvW12A&s";
+
+// console.log(img.src);
+
+// a.href =
+//   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwv178TD2B0QpRzs8RzWoGKiSZzS4HqvW12A&s";
+
+// console.log(img.attributes);
+// console.log(img.setAttribute("test", "potichat"));
+// console.log(img.getAttribute("test"));
+// img.removeAttribute("test");
+
+console.log(input.dataset.test);
+
+input.focus();
+setTimeout(() => {
+  input.blur();
+}, 1000);
+
+// input.type = "date";
+input.placeholder = "Hello";
+
+//désactiver l'input
+// input.disabled = true;
+
+// input.minlenght = 10;
+
+console.log(input.validity);
+// Mismatch: false, tooLong: false, tooShort: false, …}badInput: falsecustomError: falsepatternMismatch: falserangeOverflow: falserangeUnderflow: falsestepMismatch: falsetooLong: falsetooShort: falsetypeMismatch: falsevalid: truevalueMissing: false[[Prototype]]: ValidityState
